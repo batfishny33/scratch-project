41f7a1800c2265ed4ab165ce7dba86172fb37eab
CodedMarc <doramm4408@gmail.com>
2022-05-31 09:23:07 -0500

    fixed express bug

--- server/routes/UserRouter.js
+++ server/routes/UserRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/UserController.js');
-const express = require('express');
+
 
 
 // Search For Users (for filtering preferences)
